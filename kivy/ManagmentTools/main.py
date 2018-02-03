8f416a536c0cc3158a4b1b79846a83de70d7b6f7
Adriano Oliveira <adriano.qwe32@gmail.com>
2018-02-03 08:26:10 -0300

    main screen

--- kivy/ManagmentTools/main.py
+++ kivy/ManagmentTools/main.py
@@ -3,11 +3,24 @@ import kivy
 kivy.require("1.9.0")
 from kivy.app import App
 from screens.login import LoginSceen
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.label import Label
 
+class MainScreen(AnchorLayout):
+    pass
 
 class MTApp(App):
     def build(self):
-        return LoginSceen()
+        self.mainSreen = MainScreen()
+        self.mainSreen.add_widget(LoginSceen())
+        return self.mainSreen
+    
+    def action_btn(self, action):
+        if action == 'register': self.register()
+        
+    def register(self):
+        print("Let's go")
+
 
 if __name__ == '__main__':
     MTApp().run()
--- kivy/ManagmentTools/screens/login.py
+++ kivy/ManagmentTools/screens/login.py
@@ -8,8 +8,12 @@ Builder.load_string("""
     font_size: '50sp'
     #text_size: root.width, root.height
     #ellipsis_options: {'color':(1,0.5,0.5,1),'underline':True}
+
 <MyTextInput@TextInput>:
     multiline: False
+    background_color: [0.8, 0.9, 1, 1]
+    #foreground_color: [1, 0.9, 1, 1]
+    font_size: '20sp'
     
     
 <LoginSceen>:
@@ -17,17 +21,26 @@ Builder.load_string("""
     size_hint: (0.5, 0.5)
     spacing: 30
     pos_hint: {'center_x': .5, 'center_y': .5}
+    anchor_x: 'center'
+    anchor_y: 'center'
+    id: inputs
+    
     Title:
-        text: '[color=3344ff]Bem Vindo![/color]'
+        #text: '[color=3344ff]Bem Vindo![/color]'
+        text: 'Bem Vindo!'
         markup: True
+
     MyTextInput:
         id: email
+        hint_text: 'Seu e-mail aqui'
         
     MyTextInput:
         id: password
+        hint_text: 'Sua senha aqui'
         password: True
         
     BoxLayout:
+        id: buttons
         spacing: 10
         orientation: 'horizontal'
         Button:
@@ -36,6 +49,7 @@ Builder.load_string("""
         Button:
             id: register
             text: 'Cadastro'
+            on_press: app.action_btn('register')
 """)
 
 class LoginSceen(BoxLayout):
