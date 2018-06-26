da6d5efd1b42054f4182b270346428b854774c13
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-06-26 05:55:54 -0300

    is python slow?

--- studies/performance/test_fibonacci.py
+++ studies/performance/test_fibonacci.py
@@ -24,6 +24,18 @@ if __name__ == '__main__':
         fibonacci_cached(n)
     else:
         fibonacci(n)
-        
+     
+# no_cache version
 # python3 -m cProfile -s calls test_fibonacci.py no_cache
-
+     
+# cache version
+# python3 -m cProfile -s calls test_fibonacci.py cache
+     
+'''        
+Ncalls: The number of calls that were made to the function
+Tottime: The total time spent in seconds within this function with all sub-
+    functions excluded
+    Percall, tottime / ncalls
+Cumtime: The total time spent within this function, including sub-functions
+    Percall, cumtime / ncalls
+'''
