55f883e2f6e8c95eadd6c7877529c7581dfcec45
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-06-26 05:47:26 -0300

    test_fibonacci.py

---
+++ studies/performance/test_fibonacci.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import sys 
+import functools
+
+
+@functools.lru_cache()
+def fibonacci_cached(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci_cached(n - 1) + fibonacci_cached(n - 2)
+        
+
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+        
+
+if __name__ == '__main__':
+    n = 30
+    if sys.argv[-1] == 'cache':
+        fibonacci_cached(n)
+    else:
+        fibonacci(n)
+        
+# python3 -m cProfile -s calls test_fibonacci.py no_cache
+
