d2a9f379a6a508a07e636e9f998a0eba150c6cbc
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-07-16 07:30:02 -0300

    label_encoding

--- studies/IA/label_encoding.py
+++ studies/IA/label_encoding.py
@@ -1,13 +1,8 @@
-# -*- coding: utf-8 -*-
 from sklearn import preprocessing
 
 label_encoder = preprocessing.LabelEncoder()
-imput_class = ['audi', 'ford', 'audi', 'toyota', 'ford', 'bmw']
-label_encoder.fit(imput_class)
-for number, name in enumerate(label_encoder.classes_):
-    print(name, '-->', number)
+imput_classes = ['python', 'php', 'c++', 'c#']
+label_encoder.fit(imput_classes)
 
-labels = ['toyota', 'ford', 'audi']
-encoded_labels = label_encoder.transform(labels)
-print("\nLabels = ", labels)
-print("Encoded labels =", list(encoded_labels))
+for number, name in enumerate(label_encoder.classes_):
+    print("\n Number {}; Name: {}".format(number, name))
