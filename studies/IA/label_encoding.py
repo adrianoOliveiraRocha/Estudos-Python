271ff540428cf6944b9dd22176197443b4651a2e
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-08-01 07:28:19 -0300

    update

--- studies/IA/label_encoding.py
+++
@@ -1,8 +0,0 @@
-from sklearn import preprocessing
-
-label_encoder = preprocessing.LabelEncoder()
-imput_classes = ['python', 'php', 'c++', 'c#']
-label_encoder.fit(imput_classes)
-
-for number, name in enumerate(label_encoder.classes_):
-    print("\n Number {}; Name: {}".format(number, name))
