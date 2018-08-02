d38ed38bc3f57acdff73ee29925610c66c3bfc02
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-08-02 08:31:15 -0300

    update

--- studies/Design-Patterns/exemple1/factory_method.py
+++ studies/Design-Patterns/exemple1/factory_method.py
@@ -45,6 +45,32 @@ def connect_to(filepath):
 
 def main():
     sqlite_factory = connect_to('data/person.sq3')
+    print()
+    
+    xml_factory = connect_to('data/person.xml')
+    xml_data = xml_factory.parsed_data
+    liars = xml_data.findall(".//person[lastName='{}']".format('Liar'))
+    print('found: {} persons'.format(len(liars)))
+    for liar in liars:
+        print('first name: {}'.format(liar.find('firstName').text))
+        print('last name: {}'.format(liar.find('lastName').text))
+        [print('phone number ({}):'.format(p.attrib['type']), p.text) for p in liar.find('phoneNumbers')]
+    print()
+    
+    json_factory = connect_to('data/donut.json')
+    json_data = json_factory.parsed_data
+    print('found: {} donuts'.format(len(json_data)))
+    
+    for donut in json_data:
+        print('name: {}'.format(donut['name']))
+        print('price: ${}'.format(donut['ppu']))
+        [print('topping: {} {}'.format(t['id'], t['type'])) for t in donut['topping']]
+        
+
+if __name__ == '__main__':
+    main()
+        
+        
     
     
 
