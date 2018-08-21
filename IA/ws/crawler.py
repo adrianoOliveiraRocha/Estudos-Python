8867c9fcff7c2968fdbdedc9dccf756f1fea4ad5
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-08-21 07:49:21 -0300

    #

--- IA/ws/crawler.py
+++ IA/ws/crawler.py
@@ -4,3 +4,21 @@ from urllib.parse import urljoin
 from urllib.error import URLError, HTTPError, ContentTooShortError
 
 
+def download(url, num_retries=2):
+    try:
+        html = urllib.request.urlopen(url).read()
+    except (URLError, HTTPError, ContentTooShortError) as e:
+        print('Error: ', e.reason)
+        # not always code exists
+        try:
+            print(e.code)
+        except Exception as error:
+            print(error.args)
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                return download(url, num_retries - 1)
+
+    return html
+
+print(download('http://httpstat.us/500'))
