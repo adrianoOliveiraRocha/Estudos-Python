669d61d3d0f6a9aef794e2c906e8152b9ac7dd48
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-08-22 06:07:06 -0300

    #

--- IA/ws/crawler.py
+++ IA/ws/crawler.py
@@ -4,9 +4,17 @@ from urllib.parse import urljoin
 from urllib.error import URLError, HTTPError, ContentTooShortError
 
 
-def download(url, num_retries=2):
+def download(url, user_agent='wswp', num_retries=2, charset='utf-8'):
+    print('Downloading:', url)
+    request = urllib.request.Request(url)
+    request.add_header('User-agent', user_agent)
+
     try:
-        html = urllib.request.urlopen(url).read()
+        resp = urllib.request.urlopen(request)
+        cs = resp.headers.get_content_charset()
+        if not cs:
+            cs = charset
+        html = resp.read().decode(cs)
     except (URLError, HTTPError, ContentTooShortError) as e:
         print('Error: ', e.reason)
         # not always code exists
@@ -14,6 +22,7 @@ def download(url, num_retries=2):
             print(e.code)
         except Exception as error:
             print(error.args)
+
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
@@ -21,4 +30,12 @@ def download(url, num_retries=2):
 
     return html
 
-print(download('http://httpstat.us/500'))
+def crawl_sitemap(url):
+    sitemap = download(url)
+    links = re.findall('<loc>(.*?)</loc>', sitemap)
+    # download each link
+    for link in links:
+        html = download(link)
+
+crawl_sitemap('http://example.webscraping.com/sitemap.xml')
+
