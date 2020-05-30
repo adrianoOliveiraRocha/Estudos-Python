185c194db75fcc656458dbb81879c720eefc0700
adriano <adrianoOliveiraRocha>
2020-05-30 10:57:15 -0300

    ep

---
+++ en-phrases/modules/models.py
@@ -0,0 +1,18 @@
+class Phrase:
+	def __init__(self, phrase):
+		self.phrase = phrase
+	
+	def save(self):
+		import sqlite3
+		try:
+			conn = sqlite3.connect('db/ep.db')
+			c = conn.cursor()
+			c.execute("""INSERT INTO phrase (phrase) 
+             values('{}')""".format(self.phrase))
+			conn.commit()
+			conn.close()
+			return True
+		except Exception as e:
+			print(e)
+			return False
+		
